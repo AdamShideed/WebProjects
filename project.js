fbe4c5b7340fd92adb477cd11782fe9db2dcef51
AdamShideed <56095333+AdamShideed@users.noreply.github.com>
2020-04-18 01:13:26 -0700

    Added a sleep function for 120 milleseconds from button click to display

--- project.js
+++ project.js
@@ -1,5 +1,14 @@
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 function randomize() {
 	var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+	sleep(120);
 	document.getElementById("ColourBox").style.backgroundColor = randomColor;
 	document.getElementById("ColorText").innerHTML = randomColor;
 }
